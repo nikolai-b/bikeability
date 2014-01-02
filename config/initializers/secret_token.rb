524e3b9e8e2aac9b97069a9e69d024230d4d2534
Mark Burns <markthedeveloper@gmail.com>
2014-01-02 14:33:58 +0000

    create blank rails project with rspec

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,12 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key is used for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+# You can use `rake secret` to generate a secure secret key.
+
+# Make sure your secret_key_base is kept private
+# if you're sharing your code publicly.
+Bikeability::Application.config.secret_key_base = 'c678b071ea09307fe451cdef29b6abe3ac3e0581da58225f82a33f77d906b0dde0afb09433c40cb44f150cda90adc1f9ebd4b5fe6dbe90911f9051185bac8ffc'
---
+++ config/initializers/session_store.rb
@@ -0,0 +1,3 @@
+# Be sure to restart your server when you modify this file.
+
+Bikeability::Application.config.session_store :cookie_store, key: '_bikeability_session'
