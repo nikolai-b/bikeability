f8fff7f4ef30f1ec9e3c798bd2c700d85376b796
Mark Burns <markthedeveloper@gmail.com>
2014-01-03 11:11:21 +0000

    fix security hole with secret_key_base

--- config/initializers/secret_token.rb
+++ config/initializers/secret_token.rb
@@ -1,12 +1,14 @@
-# Be sure to restart your server when you modify this file.
+secret_key_base =
+  if Rails.env.production?
+    ENV['SECRET_KEY_BASE']
+  else
+    'c678b071ea09307fe451cdef29b6abe3ac3e0581da58225f82a33f77d906b0dde0afb09433c40cb44f150cda90adc1f9ebd4b5fe6dbe90911f9051185bac8ffc'
+  end
 
-# Your secret key is used for verifying the integrity of signed cookies.
-# If you change this key, all old signed cookies will become invalid!
+if secret_key_base.blank?
+  raise Exception.new("Missing SECRET_KEY_BASE environment variable")
+else
+  Bikeability::Application.config.secret_key_base = secret_key_base
+end
 
-# Make sure the secret is at least 30 characters and all random,
-# no regular words or you'll be exposed to dictionary attacks.
-# You can use `rake secret` to generate a secure secret key.
 
-# Make sure your secret_key_base is kept private
-# if you're sharing your code publicly.
-Bikeability::Application.config.secret_key_base = 'c678b071ea09307fe451cdef29b6abe3ac3e0581da58225f82a33f77d906b0dde0afb09433c40cb44f150cda90adc1f9ebd4b5fe6dbe90911f9051185bac8ffc'
