e0180964edb98452bb28e5b16a273ae4d716fae2
Mark Burns <markthedeveloper@gmail.com>
2014-01-03 17:27:43 +0000

    try to debug issue with heroku

--- config/initializers/secret_token.rb
+++ config/initializers/secret_token.rb
@@ -5,10 +5,12 @@ secret_key_base =
     'c678b071ea09307fe451cdef29b6abe3ac3e0581da58225f82a33f77d906b0dde0afb09433c40cb44f150cda90adc1f9ebd4b5fe6dbe90911f9051185bac8ffc'
   end
 
+puts secret_key_base
+
 if secret_key_base.blank?
   raise Exception.new("Missing SECRET_KEY_BASE environment variable")
-else
-  Bikeability::Application.config.secret_key_base = secret_key_base
 end
+
+Bikeability::Application.config.secret_key_base = secret_key_base
 
 
