a127bc849708382edfc5e4fb267044acbf4d66f5
alex-frost <nikolai.berkoff@gmail.com>
2014-01-06 13:20:59 +0000

    Update bookings_controller.rb

--- app/controllers/bookings_controller.rb
+++ app/controllers/bookings_controller.rb
@@ -1,4 +1,4 @@
-class BookingsController < ApplicationController
+class BookingsController < UnauthenticatedController
   before_action :set_booking, only: [:show, :edit, :update, :destroy]
   before_action :set_school_teacher
 
